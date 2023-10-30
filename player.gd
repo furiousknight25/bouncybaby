7421656ae019bce2154224fec95c26401c1bc94c
furiousknight25 <48182589+furiousknight25@users.noreply.github.com>
2023-10-30 08:22:06 -0500

    added controlelr support

--- player.gd
+++ player.gd
@@ -1,18 +1,13 @@
 extends RigidBody2D
 
-func _ready():
-	print("s")
-
-
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed ("move up"):
-		$LArm.apply_force(Vector2(0,-500), $LArm/Marker2D.position)
-		print (linear_velocity)
-	if Input.is_action_pressed ("move down"):
-		$RArm.apply_force(Vector2(0,500), $RArm/Marker2D.position)
-		print (linear_velocity)
-		
-
+	var l_direction = Input.get_vector('lleft', 'lright','lup', 'ldown')
+	var r_direction = Input.get_vector('rleft', 'rright','rup', 'rdown')
+	print(l_direction)
+	
+	$LArm.apply_torque(l_direction.x * 100000)
+	$RArm.apply_torque(r_direction.x * 100000)
+	
+	
